ed930a9eab669da48fe57461f59af880e44b75fa
EdgarWi1 <edgarsilvanus12@gmail.com>
2025-02-08 22:37:24 +0800

    feat: peta desa image

--- resources/views/pages/petaDigital.blade.php
+++ resources/views/pages/petaDigital.blade.php
@@ -11,18 +11,10 @@
                                 class="text-xl sm:text-4xl font-semibold hover:text-indigo-600 transition duration-500 ease-in-out inline-block mb-2">
                                 Peta Desa Bontomanai</a>
 
-                        {{-- <div class="relative">
-                                <img class="w-full"
-                                        src="https://images.pexels.com/photos/5120892/pexels-photo-5120892.jpeg?auto=compress&amp;cs=tinysrgb&amp;fit=crop&amp;h=625.0&amp;sharp=10&amp;w=1500"
-                                        alt="Sunset in the mountains">
-                        </div> --}}
+                        <div class="relative">
+                                <img class="w-full" src="{{ asset('images/petaDesa/peta_desa.png') }}" alt="Peta Desa">
+                        </div>
                         <p class="text-gray-700 py-5 text-lg leading-8">
-                                Machu Picchu is a 15th-century Inca citadel situated on a mountain ridge 2,430 metres (7,970 ft)
-                                above sea
-                                level. It is located in the Cusco Region, Urubamba Province, Machupicchu District in Peru, above
-                                the Sacred
-                                Valley, which is 80 kilometres (50 mi) northwest of Cuzco and through which the Urubamba River
-                                flows.
                         </p>
                 </div>
 
